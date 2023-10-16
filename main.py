8231a9c9805a3839208f20dc9b6274f8acb92e60
Joshua Erhahon <joshua.erhahon@riversafe.co.uk>
2023-10-16 10:46:17 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# get user input for their name
+
+# print their name to console "Hi, [users name]"
+
+#Just the hello world
+print('Hello world!')
+
+#use input() to let the user assign a value to a variable named myName
+myName = input('What is your name?:')
+
+#Use the Range function to repeat the names
+print('It is nice to meet you, ' + myName)
